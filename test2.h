034c373207c3d4225642c8a8c647f7b73234dc61
QHXRPG <105186795+QHXRPG@users.noreply.github.com>
2023-03-08 21:06:07 +0800

    day3

---
+++ test2.h
@@ -0,0 +1,11 @@
+//
+// Created by 邱浩轩 on 2023/3/8.
+//
+
+#ifndef OPENCV3_DEMO_TEST2_H
+#define OPENCV3_DEMO_TEST2_H
+
+#endif //OPENCV3_DEMO_TEST2_H
+void test2_1();
+void test2_2();
+void test2_3();
---
+++ test3.h
@@ -0,0 +1,10 @@
+//
+// Created by 邱浩轩 on 2023/3/8.
+//
+
+#ifndef OPENCV3_DEMO_TEST3_H
+#define OPENCV3_DEMO_TEST3_H
+
+#endif //OPENCV3_DEMO_TEST3_H
+void test3_1();
+void test3_2();
